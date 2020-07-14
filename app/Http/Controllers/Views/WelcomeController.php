1df26af8910df74a38f0b9a42f69bf02542b4b1c
VictorMaderaA <victor.madera.a@gmail.com>
2020-07-14 09:12:58 -0700

    Base Proyect

---
+++ app/Http/Controllers/Views/WelcomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Views;
+
+use App\Http\Controllers\Base\Controller;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+
+    public function __invoke(){
+        return view('web.welcome');
+    }
+
+}
