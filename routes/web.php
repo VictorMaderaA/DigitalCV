a469f3fefd6eabef3fecd129dd178c06f88634f9
VictorMaderaA <victor.madera.a@gmail.com>
2020-07-15 09:06:39 -0700

    Init

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes(['verify' => true]);
+
+
+Route::get('/', 'Views\WelcomeController');
+Route::get('/home', 'Views\HomeController@index')->middleware('verified');
